549df3254890bac146cc69479e10c77e504431a9
YuLe0406 <1221207303@student.mmu.edu.my>
2025-03-25 15:49:04 +0800

    0325

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ echo '
         <button type="submit"><i class="fas fa-search"></i></button>
     </div>
     <div class="icons">
-        <a href="login.php" class="icon"><i class="fas fa-user"></i> Login / Register</a>
+        <a href="login.html" class="icon"><i class="fas fa-user"></i> Login / Register</a>
         <a href="#" class="icon"><i class="fas fa-heart"></i></a>
         <a href="cart.php" class="icon"><i class="fas fa-shopping-cart"></i></a>
     </div>
