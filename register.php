74a5170abd2f1f46d6b6f601d0d7001a2b61816e
Yaw Shi Hao <1221207701@soffice.mmu.edu.my>
2025-04-15 01:48:52 +0800

    15/4

--- register.php
+++ register.php
@@ -60,36 +60,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 }
-// 获取数据（带默认值）
-$firstName = $_POST['first_name'] ?? '';
-$lastName  = $_POST['last_name'] ?? '';
-$email     = $_POST['email'] ?? '';
-$password  = $_POST['password'] ?? '';
-$phone     = $_POST['phone'] ?? '';
-$dob       = $_POST['dob'] ?? '';
-$gender    = $_POST['gender'] ?? '';
-
-// 必填字段验证
-$errors = [];
-if (empty($firstName)) $errors[] = "First name is required";
-if (empty($lastName))  $errors[] = "Last name is required";
-if (empty($email))     $errors[] = "Email is required";
-if (empty($password))  $errors[] = "Password is required";
-if (empty($phone))     $errors[] = "Phone number is required";
-if (empty($dob))       $errors[] = "Date of birth is required";
-if (empty($gender))    $errors[] = "Gender is required";
-
-// 邮箱格式验证
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = "Invalid email format";
-}
-
-// 手机号格式验证
-if (!preg_match('/^[0-9]{11}$/', $phone)) {
-    $errors[] = "Phone number must be 11 digits";
-}
-
-// 如果有错误则终止
-if (!empty($errors)) {
-    die("Registration failed:<br>" . implode("<br>", $errors));
-}
