50209578083f8bb4db442f12502c2062a282fe4f
Yaw Shi Hao <1221207701@soffice.mmu.edu.my>
2025-04-25 01:32:10 +0800

    25/4

--- profile.php
+++ profile.php
@@ -43,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile | CTRL+X</title>
     <link rel="stylesheet" href="auth.css">
+    <link rel="stylesheet" href="register.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
